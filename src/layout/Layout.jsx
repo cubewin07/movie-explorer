af95cab7f44fa37225c543f1b763ba1c5e7e5349
cubewin <tanthang071208@gmail.com>
2025-06-20 13:26:43 +0700

    Fixing my layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,13 +2,13 @@ import Sidebar from "../components/react_components/Sidebar";
 import { Outlet } from "react-router-dom";
 function Layout() {
     return (  
-        <div className="flex h-screen max-w-screen-2xl mx-auto">
+        <>
             <Sidebar />
             <main>
                 <Outlet />
             </main>
             <Sidebar />
-        </div>
+        </>
     );
 }
 
