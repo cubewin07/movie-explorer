6644e62b1b9d38e91e1137212c2a50f4b3f17c48
cubewin <tanthang071208@gmail.com>
2025-06-20 14:23:05 +0700

    Apply successfully css on menu item

--- src/components/react_components/Sidebar.jsx
+++ src/components/react_components/Sidebar.jsx
@@ -6,9 +6,9 @@ function Sidebar({}) {
                 {/* <img src={logo} alt="logo" /> */}
             </div>
             <div className="flex flex-col items-center justify-between">
-                <ul className="menu w-full">
+                <ul className="menu menu-border w-full pr-0 ">
                     <li className="menu-title">Menu</li>
-                    <li className="menu-active">
+                    <li>
                         <Link to="/"> 
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ function Sidebar({}) {
                                     strokeLinejoin="round"
                                     strokeWidth="2"
                                     d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" 
-                                />
+                                    />
                             </svg>
                                 Home
                             </Link>
@@ -30,13 +30,13 @@ function Sidebar({}) {
                     <li><Link to="/discovery">Discovery</Link></li>
                     <li><Link to="/coming-soon">Coming Soon</Link></li>
                 </ul>
-                <ul className="menu w-full">
+                <ul className="menu w-full pr-0">
                     <li className="menu-title">Social</li>
                     <li><Link to="/profile">Profile</Link></li>
                     <li><Link to="/friend">Friend</Link></li>
                     <li><Link to="/media">Media</Link></li>
                 </ul>
-                <ul className="menu w-full">
+                <ul className="menu w-full pr-0">
                     <li className="menu-title">General</li>
                     <li><Link to="/settings">Settings</Link></li>
                     <li><Link to="/logout">Logout</Link></li>
