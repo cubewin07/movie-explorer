008f4f54cf2f33c90983458c551812bb48c1817c
cubewin <tanthang071208@gmail.com>
2025-06-22 20:30:05 +0700

    Using popular component for popular movies

--- src/components/react_components/SideBar/Popular/PopularMovies.jsx
+++ src/components/react_components/SideBar/Popular/PopularMovies.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import clsx from 'clsx';
-
 import axiosInstance from '@/lib/axiosInstance';
 import LoadingSideBar from '../Popular/LoadingSideBar';
 import Popular from '../Popular/Popular';
@@ -11,10 +8,7 @@ function PopularMovies() {
         queryKey: ['popularMovies'],
         queryFn: () =>
             axiosInstance.get('/movie/popular', {
-                params: {
-                    language: 'en-US',
-                    page: 1,
-                },
+                params: { language: 'en-US', page: 1 },
             }),
         refetchOnWindowFocus: false,
         refetchOnReconnect: false,
@@ -25,55 +19,23 @@ function PopularMovies() {
         queryKey: ['MovieGenres'],
         queryFn: () =>
             axiosInstance.get('/genre/movie/list', {
-                params: {
-                    language: 'en-US',
-                },
+                params: { language: 'en-US' },
             }),
         refetchOnWindowFocus: false,
         refetchOnReconnect: false,
         staleTime: Infinity,
     });
 
-    console.log(genres?.data);
-
-    console.log(popularMovies?.data?.results);
-
-    const movies = popularMovies?.data?.results || [];
-    const visibleMovies = movies.slice(0, 3);
+    const movies = popularMovies?.data?.results?.slice(0, 3) || [];
+    const genreList = genres?.data?.genres || [];
 
     return (
         <div>
             <h1 className="text-white text-opacity-50 text-lg font-bold mb-3">Popular Movies</h1>
+
             {(isPopularMoviesLoading || isGenresLoading) && <LoadingSideBar />}
-            {!isPopularMoviesLoading && !isGenresLoading && (
-                <div className="flex flex-col gap-4">
-                    {visibleMovies.map((movie) => (
-                        <div key={movie.id} className="flex gap-4 items-start cursor-pointer">
-                            <img
-                                src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
-                                alt={movie.title}
-                                className="h-24 rounded-lg object-cover"
-                            />
-                            <div className="flex flex-col justify-between h-24">
-                                <div>
-                                    <h2 className="text-white text-xs font-semibold w-[100px] truncate whitespace-nowrap overflow-hidden">
-                                        {movie.title || movie.name}
-                                    </h2>
-                                    <p className="text-[10px] text-neutral-200">{movie.release_date}</p>
-                                </div>
-                                <div className="flex items-center gap-1">
-                                    <div className="bg-yellow-400 text-black text-[10px] font-extrabold px-1.5  rounded-sm">
-                                        IMDb
-                                    </div>
-                                    <span className="text-xs text-slate-300 font-medium">
-                                        {movie.vote_average.toFixed(1)}
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            )}
+
+            {!isPopularMoviesLoading && !isGenresLoading && <Popular movies={movies} genres={genreList} />}
         </div>
     );
 }
