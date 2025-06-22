454cc1108f6f30a7643e73633e6426112615e61c
cubewin <tanthang071208@gmail.com>
2025-06-22 20:35:54 +0700

    changing color for title

--- src/components/react_components/SideBar/Popular/Popular.jsx
+++ src/components/react_components/SideBar/Popular/Popular.jsx
@@ -8,8 +8,7 @@ function Popular({ movies, genres }) {
     };
     return (
         <div className="flex flex-col gap-4">
-            {movies.map((movie, index) => {
-                console.log(movie);
+            {movies.map((movie) => {
                 const genreNames = getGenreNames(movie.genre_ids);
                 return (
                     <div key={movie.id} className="flex gap-4 items-start">
@@ -20,17 +19,17 @@ function Popular({ movies, genres }) {
                         />
                         <div className="flex flex-col justify-between h-24">
                             <div>
-                                <h2 className="text-white text-xs font-semibold w-[100px] truncate whitespace-nowrap overflow-hidden">
+                                <h2 className="text-gray-900 text-xs font-semibold w-[100px] truncate whitespace-nowrap overflow-hidden">
                                     {movie.title || movie.name}
                                 </h2>
-                                <p className="text-[10px] text-neutral-200">
+                                <p className="text-[10px] text-gray-700">
                                     {movie.release_date || movie.first_air_date}
                                 </p>
                                 <div className="flex flex-wrap gap-1 mt-1 items-center">
                                     {genreNames.map((name) => (
                                         <span
                                             key={name}
-                                            className="text-[10px] px-1.5 py-[1px] rounded font-medium bg-blue-100 text-blue-700 dark:bg-violet-900 dark:text-violet-200"
+                                            className="text-[10px] px-1.5 py-[1px] rounded font-medium bg-blue-200 text-blue-800"
                                         >
                                             {name}
                                         </span>
@@ -38,10 +37,10 @@ function Popular({ movies, genres }) {
                                 </div>
                             </div>
                             <div className="flex items-center gap-1">
-                                <div className="bg-yellow-400 text-black text-[10px] font-extrabold px-1.5  rounded-sm">
+                                <div className="bg-yellow-400 text-black text-[10px] font-extrabold px-1.5 rounded-sm">
                                     IMDb
                                 </div>
-                                <span className="text-xs text-slate-300 font-medium">
+                                <span className="text-xs text-gray-800 font-medium">
                                     {movie.vote_average.toFixed(1)}
                                 </span>
                             </div>
