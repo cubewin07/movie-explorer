e736a21d756a28d651a12c1e57cc84fa2f940555
cubewin <tanthang071208@gmail.com>
2025-06-23 09:37:53 +0700

    Create context for showing modal

---
+++ src/context/FilmModalProvider.jsx
@@ -0,0 +1,19 @@
+import { useState, createContext } from 'react';
+
+import MovieReviewModalDemo from '@/components/react_components/Modal/ReviewFilms';
+
+const FilmModalContext = createContext();
+
+function FilmModalProvider({ children }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [content, setContext] = useState({});
+
+    return (
+        <FilmModalContext.Provider value={{ setIsOpen, setContext }}>
+            {children}
+            {isOpen && <MovieReviewModalDemo {...content} />}
+        </FilmModalContext.Provider>
+    );
+}
+
+export default FilmModalProvider;
