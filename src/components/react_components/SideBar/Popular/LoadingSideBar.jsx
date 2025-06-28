c17570ecdca3d413559a1d390aed4a26d8b26888
cubewin <tanthang071208@gmail.com>
2025-06-28 10:18:42 +0700

    Reponsive loading sidebar

--- src/components/react_components/SideBar/Popular/LoadingSideBar.jsx
+++ src/components/react_components/SideBar/Popular/LoadingSideBar.jsx
@@ -1,24 +1,24 @@
 function LoadingSideBar() {
     return (
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-3 sm:gap-5">
             {Array(3)
                 .fill(0)
                 .map((_, i) => (
-                    <div key={i} className="flex gap-4">
+                    <div key={i} className="flex gap-3 sm:gap-4">
                         {/* Poster skeleton */}
-                        <div className="w-24 h-24 rounded-lg skeleton" />
+                        <div className="w-20 h-20 sm:w-24 sm:h-24 rounded-lg skeleton" />
 
                         {/* Text content */}
                         <div className="flex flex-col justify-between w-full">
                             <div className="space-y-2">
-                                <div className="h-4 w-2/3 skeleton rounded" />
+                                <div className="h-3 sm:h-4 w-2/3 skeleton rounded" />
                                 <div className="h-2 w-1/2 skeleton rounded" />
                             </div>
-                            <div className="h-5 w-16 skeleton rounded" />
+                            <div className="h-4 sm:h-5 w-14 sm:w-16 skeleton rounded" />
                         </div>
                     </div>
                 ))}
-            <div className="h-8 w-full skeleton rounded" />
+            <div className="h-6 sm:h-8 w-full skeleton rounded" />
         </div>
     );
 }
