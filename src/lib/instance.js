45e42299621c3a8fa8420ed41dec7e444e9a680c
cubewin <tanthang071208@gmail.com>
2025-07-17 19:20:13 +0700

    FIxing logic

--- src/lib/instance.js
+++ src/lib/instance.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+const token = Cookies.get('token');
+
 const instance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://moviehub.congminh.site/api/',
+    timeout: 10000, // 10 seconds
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token ? `Bearer ${token}` : undefined,
+    },
 });
 
 export default instance;
