f3afda11b16129b82b605515f2175664e1f1fae4
cubewin <tanthang071208@gmail.com>
2025-09-11 20:53:34 +0700

    adding cors in config

--- Backend/src/main/java/com/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/Backend/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.Backend.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,17 +12,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.web.cors.*;
+import org.springframework.security.config.Customizer;
+
 
 import lombok.RequiredArgsConstructor;
 
 import com.Backend.springSecurity.jwtAuthentication.JwtFilterChain;
 
 
+
 @Configuration  
 @RequiredArgsConstructor
 public class SecurityConfig {
-    
+
     private final JwtFilterChain jwtFilterChain;
     private final UserDetailsService userDetailsService;
 
@@ -30,11 +37,25 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated()
             )
+            .cors(Customizer.withDefaults())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authenticationProvider(authenticationProvider(userDetailsService))
             .addFilterBefore(jwtFilterChain, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }   
+
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
