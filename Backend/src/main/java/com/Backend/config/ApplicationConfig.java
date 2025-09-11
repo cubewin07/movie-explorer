81dcca1c9c3d91a48c98172d408fd49727c97593
cubewin <tanthang071208@gmail.com>
2025-09-11 20:20:42 +0700

    Create userDetailsService bean

---
+++ Backend/src/main/java/com/Backend/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package com.Backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.Backend.user_service.repository.UserRepository;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return email -> userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+    
+}
