bb4abe7559e0e9f2edfb9ea5fa766a0323713d31
cubewin <tanthang071208@gmail.com>
2025-09-11 19:28:48 +0700

    Create User entity

---
+++ Backend/src/main/java/com/Backend/user_service/model/ROLE.java
@@ -0,0 +1,5 @@
+package com.Backend.user_service.model;
+
+public enum ROLE {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ Backend/src/main/java/com/Backend/user_service/model/User.java
@@ -0,0 +1,49 @@
+package com.Backend.user_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+public class User implements UserDetails {
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    private Long id;
+    @NotNull(message = "Username cannot be null")
+    private String username;
+    @NotNull(message = "Email cannot be null")
+    private String email;
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private ROLE role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
