8dfaaf3c469ee43e40fc910bdeb517a8d1a0a3eb
cubewin <tanthang071208@gmail.com>
2025-09-11 20:46:27 +0700

    Created securityConfig

--- Backend/src/main/java/com/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/Backend/config/SecurityConfig.java
@@ -2,13 +2,16 @@ package com.Backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import com.Backend.springSecurity.jwtAuthentication.JwtService;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import com.Backend.springSecurity.jwtAuthentication.JwtFilterChain;
@@ -17,7 +20,10 @@ import com.Backend.springSecurity.jwtAuthentication.JwtFilterChain;
 @Configuration  
 @RequiredArgsConstructor
 public class SecurityConfig {
+    
     private final JwtFilterChain jwtFilterChain;
+    private final UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -25,10 +31,20 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider()
+            .authenticationProvider(authenticationProvider(userDetailsService))
             .addFilterBefore(jwtFilterChain, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+            daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+            daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return  daoAuthenticationProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
