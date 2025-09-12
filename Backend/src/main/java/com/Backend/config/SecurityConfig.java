3c9b1704a7fac337d6a42fb80ebb594eb1ca9103
cubewin <tanthang071208@gmail.com>
2025-09-12 14:18:58 +0700

    Adding permitted endpoints

--- Backend/src/main/java/com/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/Backend/config/SecurityConfig.java
@@ -34,7 +34,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+            .authorizeHttpRequests(auth ->
+                    auth
+                            .requestMatchers("/users/register").permitAll()
+                            .requestMatchers("/users/authenticate").permitAll()
                 .anyRequest().authenticated()
             )
             .cors(Customizer.withDefaults())
