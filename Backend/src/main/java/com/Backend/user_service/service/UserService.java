d140f57cac117ef0419554b8b180bdf48a5c7377
cubewin <tanthang071208@gmail.com>
2025-09-13 20:23:48 +0700

    Adding service

--- Backend/src/main/java/com/Backend/user_service/service/UserService.java
+++ Backend/src/main/java/com/Backend/user_service/service/UserService.java
@@ -8,12 +8,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
