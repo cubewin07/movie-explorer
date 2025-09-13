bb81e0eb533172c22ba138f132106309dccda595
cubewin <tanthang071208@gmail.com>
2025-09-13 09:33:09 +0700

    Create my first endpoint

--- Backend/src/main/java/com/Backend/user_service/controller/UserController.java
+++ Backend/src/main/java/com/Backend/user_service/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.Backend.user_service.controller;
 
+import com.Backend.user_service.model.User;
+import com.Backend.user_service.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserController {
-
+    private final UserService userService;
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
 }
