93acbe3b55d6c3f49d52403fedc828b60def0259
cubewin <tanthang071208@gmail.com>
2025-09-13 20:27:14 +0700

    Adding no argsconstructor for user

--- Backend/src/main/java/com/Backend/user_service/model/User.java
+++ Backend/src/main/java/com/Backend/user_service/model/User.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +18,7 @@ import java.util.List;
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
 public class User implements UserDetails {
