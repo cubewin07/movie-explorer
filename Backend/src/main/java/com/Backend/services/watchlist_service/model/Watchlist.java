18ca03f54c9ed905f9f17d2d0c40aa5911929043
cubewin <tanthang071208@gmail.com>
2025-09-15 20:19:17 +0700

    Draft watchlist entity

---
+++ Backend/src/main/java/com/Backend/services/watchlist_service/model/Watchlist.java
@@ -0,0 +1,32 @@
+package com.Backend.services.watchlist_service.model;
+
+import com.Backend.services.user_service.model.User;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Watchlist {
+
+    @Id
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ElementCollection
+    @CollectionTable(name = "watchlist_series", joinColumns = @JoinColumn(name = "watchlist_id"))
+    @Column(name = "series_id")
+    private List<Long> seriesId;
+
+    @ElementCollection
+    @CollectionTable(name = "watchlist_movies", joinColumns = @JoinColumn(name = "watchlist_id"))
+    @Column(name = "movie_id")
+    private List<Long> moviesId;
+}
