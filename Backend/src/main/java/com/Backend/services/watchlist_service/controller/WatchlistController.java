93e24beca2e5f22f1fae1bdcf437cac314dacda2
cubewin <tanthang071208@gmail.com>
2025-09-16 19:25:31 +0700

    Create endpoints for get watchlist

--- Backend/src/main/java/com/Backend/services/watchlist_service/controller/WatchlistController.java
+++ Backend/src/main/java/com/Backend/services/watchlist_service/controller/WatchlistController.java
@@ -1,13 +1,22 @@
 package com.Backend.services.watchlist_service.controller;
 
+import com.Backend.services.user_service.model.User;
+import com.Backend.services.watchlist_service.model.Watchlist;
+import com.Backend.services.watchlist_service.service.WatchlistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/watchlist")
+@RequiredArgsConstructor
 public class WatchlistController {
+    private final WatchlistService watchlistService;
+
     @GetMapping()
-    public String getWatchlist() {
-        return "Watchlist";
+    public ResponseEntity<Watchlist> getWatchlist(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(watchlistService.getWatchlist(user));
     }
     @PostMapping
     public String addMovieToWatchlist() {
