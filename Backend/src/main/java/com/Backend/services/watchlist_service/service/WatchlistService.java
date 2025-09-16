c69e8b6071cba7c942a6e7becb6de64f4eb6b05a
cubewin <tanthang071208@gmail.com>
2025-09-16 09:25:14 +0700

    Create class for wathclist service

---
+++ Backend/src/main/java/com/Backend/services/watchlist_service/service/WatchlistService.java
@@ -0,0 +1,9 @@
+package com.Backend.services.watchlist_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WatchlistService {
+}
