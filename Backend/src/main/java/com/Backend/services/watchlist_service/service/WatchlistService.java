984278c49c1dc60cb9e16451ac08403909e973f8
cubewin <tanthang071208@gmail.com>
2025-09-16 19:17:47 +0700

    Create endpoint for get watchlist

--- Backend/src/main/java/com/Backend/services/watchlist_service/service/WatchlistService.java
+++ Backend/src/main/java/com/Backend/services/watchlist_service/service/WatchlistService.java
@@ -1,9 +1,17 @@
 package com.Backend.services.watchlist_service.service;
 
+import com.Backend.services.user_service.model.User;
+import com.Backend.services.watchlist_service.model.Watchlist;
+import com.Backend.services.watchlist_service.repository.WatchlistRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class WatchlistService {
+
+    private final WatchlistRepository watchlistRepository;
+    public Watchlist getWatchlist(User user) {
+        return watchlistRepository.findByUserId(user.getId()).orElseThrow();
+    }
 }
