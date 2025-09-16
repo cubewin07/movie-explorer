5c7fce050dade53d84824f75666b82246021745c
cubewin <tanthang071208@gmail.com>
2025-09-16 19:19:31 +0700

    Create adding to watchlist

--- Backend/src/main/java/com/Backend/services/watchlist_service/service/WatchlistService.java
+++ Backend/src/main/java/com/Backend/services/watchlist_service/service/WatchlistService.java
@@ -11,7 +11,21 @@ import org.springframework.stereotype.Service;
 public class WatchlistService {
 
     private final WatchlistRepository watchlistRepository;
+
     public Watchlist getWatchlist(User user) {
         return watchlistRepository.findByUserId(user.getId()).orElseThrow();
     }
+
+    public void addMovieToWatchlist(Long movieId, User user) {
+        Watchlist watchlist = getWatchlist(user);
+        watchlist.getMoviesId().add(movieId);
+        watchlistRepository.save(watchlist);
+    }
+
+    public void addSeriesToWatchlist(Long seriesId, User user) {
+        Watchlist watchlist = getWatchlist(user);
+        watchlist.getSeriesId().add(seriesId);
+        watchlistRepository.save(watchlist);
+    }
+
 }
