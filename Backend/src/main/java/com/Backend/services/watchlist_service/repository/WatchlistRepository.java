29867a3e5323686c7cac965544510c2367a0c2c3
cubewin <tanthang071208@gmail.com>
2025-09-16 10:13:28 +0700

    Adding findUserBy movied Id

--- Backend/src/main/java/com/Backend/services/watchlist_service/repository/WatchlistRepository.java
+++ Backend/src/main/java/com/Backend/services/watchlist_service/repository/WatchlistRepository.java
@@ -3,10 +3,16 @@ package com.Backend.services.watchlist_service.repository;
 import com.Backend.services.watchlist_service.model.Watchlist;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
     @EntityGraph(attributePaths = {"seriesId", "moviesId"})
     Optional<Watchlist> findByUserId(Long userId);
+
+    @Query("SELECT w FROM Watchlist w JOIN w.moviesId m WHERE m = :movieId")
+    Optional<List<Watchlist>> findByMoviesId(@Param("movieId")Long movieId);
 }
