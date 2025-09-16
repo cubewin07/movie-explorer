aec61a99337b2a7154f7b9af61bba87700803c32
cubewin <tanthang071208@gmail.com>
2025-09-16 10:13:57 +0700

    SeriesId

--- Backend/src/main/java/com/Backend/services/watchlist_service/repository/WatchlistRepository.java
+++ Backend/src/main/java/com/Backend/services/watchlist_service/repository/WatchlistRepository.java
@@ -10,9 +10,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+
     @EntityGraph(attributePaths = {"seriesId", "moviesId"})
     Optional<Watchlist> findByUserId(Long userId);
 
     @Query("SELECT w FROM Watchlist w JOIN w.moviesId m WHERE m = :movieId")
     Optional<List<Watchlist>> findByMoviesId(@Param("movieId")Long movieId);
+
+    @Query("SELECT w FROM Watchlist w JOIN w.seriesId s WHERE s = :seriesId")
+    Optional<List<Watchlist>> findBySeriesId(@Param("seriesId")Long seriesId);
 }
