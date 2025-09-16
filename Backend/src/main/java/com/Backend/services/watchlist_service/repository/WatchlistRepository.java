6e242a9bfdf5b07cb20e2bd6d1b0c156c7beb4b6
cubewin <tanthang071208@gmail.com>
2025-09-16 09:43:54 +0700

    Using entity graph

--- Backend/src/main/java/com/Backend/services/watchlist_service/repository/WatchlistRepository.java
+++ Backend/src/main/java/com/Backend/services/watchlist_service/repository/WatchlistRepository.java
@@ -1,10 +1,12 @@
 package com.Backend.services.watchlist_service.repository;
 
 import com.Backend.services.watchlist_service.model.Watchlist;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+    @EntityGraph(attributePaths = {"seriesId", "moviesId"})
     Optional<Watchlist> findByUserId(Long userId);
 }
