f81d765c99e63e979748cef14ae0ebfad65505b9
cubewin <tanthang071208@gmail.com>
2025-09-17 19:36:54 +0700

    Created multi level cahceManager

--- Backend/src/main/java/com/Backend/cache/Multilevel_CacheManager.java
+++ Backend/src/main/java/com/Backend/cache/Multilevel_CacheManager.java
@@ -1,4 +1,33 @@
 package com.Backend.cache;
 
-public class Multilevel_CacheManager {
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@RequiredArgsConstructor
+public class Multilevel_CacheManager implements CacheManager {
+    private final CacheManager redis;
+    private final CacheManager caffeine;
+    private Map<String, Cache> cacheMap = new ConcurrentHashMap<>();
+
+    @Override
+    public Cache getCache(String name) {
+        return cacheMap.computeIfAbsent(name, cacheName -> {
+            Cache remote = redis.getCache(cacheName);
+            Cache local = caffeine.getCache(cacheName);
+            return new Multilevel_Cache(remote, local);
+        });
+    }
+
+    @Override
+    public Collection<String> getCacheNames() {
+        return redis.getCacheNames();
+    }
 }
