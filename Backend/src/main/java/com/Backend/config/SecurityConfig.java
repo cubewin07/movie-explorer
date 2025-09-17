d2ddd5a95e6945eac1547862dd67e57304e4cf61
cubewin <tanthang071208@gmail.com>
2025-09-17 20:35:05 +0700

    Change permit endpoint

--- Backend/src/main/java/com/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/Backend/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth ->
                     auth
-                            .requestMatchers("POST", "/users").permitAll()
+                            .requestMatchers("/users/register").permitAll()
                             .requestMatchers("/users/authenticate").permitAll()
                 .anyRequest().authenticated()
             )
