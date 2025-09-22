ab087075d5f7a73874ce81f035f38fca7082bc07
cubewin <tanthang071208@gmail.com>
2025-09-22 20:26:41 +0700

    Create friend entity

---
+++ Backend/src/main/java/com/Backend/services/friend_service/model/friend.java
@@ -0,0 +1,43 @@
+package com.Backend.services.friend_service.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.Backend.services.user_service.model.User;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder    
+@Data
+public class friend {
+    @EmbeddedId
+    private friendIdEb id;
+
+    @MapsId("user1")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user1_id")
+    private User user1;
+    
+    @MapsId("user2")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user2_id")
+    private User user2;
+    
+    private String status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
