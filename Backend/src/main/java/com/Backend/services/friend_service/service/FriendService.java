8962d671c014cd3d0bad1ba323a5bde6a4d79c3d
cubewin <tanthang071208@gmail.com>
2025-09-23 20:07:11 +0700

    Created getfriend service

--- Backend/src/main/java/com/Backend/services/friend_service/service/FriendService.java
+++ Backend/src/main/java/com/Backend/services/friend_service/service/FriendService.java
@@ -1,6 +1,7 @@
 package com.Backend.services.friend_service.service;
 
 
+import com.Backend.services.friend_service.model.Friend;
 import com.Backend.services.friend_service.repository.FriendRepo;
 import com.Backend.services.user_service.model.User;
 import com.Backend.services.user_service.repository.UserRepository;
@@ -8,13 +9,17 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class FriendService {
     private final FriendRepo friendRepo;
     private final UserRepository userRepository;
 
-    public void getFriend(Long id) {
+    public Set<Friend> getFriend(Long id) {
+        User user = userRepository.findByIdwithFriendlistNoRequest(id).orElseThrow();
+        return user.getFriends();
     }
 
 }
