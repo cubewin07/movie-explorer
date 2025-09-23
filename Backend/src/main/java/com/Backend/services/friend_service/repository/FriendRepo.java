ce068b64a69a4ba58c26c92b581d0ad3c51b00ac
cubewin <tanthang071208@gmail.com>
2025-09-23 20:45:51 +0700

    Create method to find friend using user1 and user 2

--- Backend/src/main/java/com/Backend/services/friend_service/repository/FriendRepo.java
+++ Backend/src/main/java/com/Backend/services/friend_service/repository/FriendRepo.java
@@ -2,7 +2,11 @@ package com.Backend.services.friend_service.repository;
 
 import com.Backend.services.friend_service.model.Friend;
 import com.Backend.services.friend_service.model.FriendIdEb;
+import com.Backend.services.user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FriendRepo extends JpaRepository<Friend, FriendIdEb> {
+    Optional<Friend> findByUser1AndUser2(User user1, User user2);
 }
