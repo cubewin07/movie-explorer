b2789aff7fc695b78d549f1d3c6ae41aa7a681a4
cubewin <tanthang071208@gmail.com>
2025-09-24 19:20:24 +0700

    Upgrade my friend repository

--- Backend/src/main/java/com/Backend/services/friend_service/repository/FriendRepo.java
+++ Backend/src/main/java/com/Backend/services/friend_service/repository/FriendRepo.java
@@ -2,11 +2,19 @@ package com.Backend.services.friend_service.repository;
 
 import com.Backend.services.friend_service.model.Friend;
 import com.Backend.services.friend_service.model.FriendIdEb;
+import com.Backend.services.friend_service.model.Status;
 import com.Backend.services.user_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FriendRepo extends JpaRepository<Friend, FriendIdEb> {
     Optional<Friend> findByUser1AndUser2(User user1, User user2);
+
+    List<Friend> findByUser1AndStatus(User user1, Status status);
+    List<Friend> findByUser2AndStatus(User user2, Status status);
+
+    boolean existsByUser1AndUser2(User user1, User user2);
+    boolean existsByUser2AndUser1(User user2, User user1);
 }
