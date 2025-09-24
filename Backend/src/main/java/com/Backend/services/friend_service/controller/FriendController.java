972b863da8494aa5d5493affa0ce7cfa8a590097
cubewin <tanthang071208@gmail.com>
2025-09-24 14:38:21 +0700

    Endpoint for status

--- Backend/src/main/java/com/Backend/services/friend_service/controller/FriendController.java
+++ Backend/src/main/java/com/Backend/services/friend_service/controller/FriendController.java
@@ -3,12 +3,15 @@ package com.Backend.services.friend_service.controller;
 import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import com.Backend.services.friend_service.model.Friend;
+import com.Backend.services.friend_service.model.Status;
 import com.Backend.services.friend_service.service.FriendService;
+import com.Backend.services.user_service.model.User;
 
 @RestController
 @RequestMapping("/friends")
@@ -25,5 +28,10 @@ public class FriendController {
     @GetMapping("/requestsTo")
     public ResponseEntity<Set<Friend>> getRequestsToThisUser(Long id) {
         return ResponseEntity.ok(friendService.getRequestsToThisUser(id));
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<Status> getFriendStatus(String friendEmail, @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(friendService.getFriendStatus(user, friendEmail));
     }
 }
