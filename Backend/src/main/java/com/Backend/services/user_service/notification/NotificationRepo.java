3befe1a842808cca191e620773887c5027117960
cubewin <tanthang071208@gmail.com>
2025-09-28 10:18:20 +0700

    create notification repository

---
+++ Backend/src/main/java/com/Backend/services/user_service/notification/NotificationRepo.java
@@ -0,0 +1,15 @@
+package com.Backend.services.user_service.notification;
+
+import java.util.List;
+
+import com.Backend.services.user_service.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepo extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByUser(User user);
+
+    List<Notification> findByUserAndIsRead(User user, boolean isRead);
+    
+}
