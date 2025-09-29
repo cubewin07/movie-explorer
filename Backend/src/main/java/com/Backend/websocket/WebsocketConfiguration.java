8b9c1b7fac2f5c4237ef2e9a533448ff84a424ca
cubewin <tanthang071208@gmail.com>
2025-09-29 19:12:04 +0700

    Adding handshaker

--- Backend/src/main/java/com/Backend/websocket/WebsocketConfiguration.java
+++ Backend/src/main/java/com/Backend/websocket/WebsocketConfiguration.java
@@ -4,12 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.Backend.websocket.interceptor.WebsocketHandshaker;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.lang.NonNull;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    private final WebsocketHandshaker websocketHandshaker;
     
     @Override
     public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
@@ -21,6 +29,7 @@ public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
         .setAllowedOrigins("*")
+        .addInterceptors(websocketHandshaker)
         .withSockJS();
     }
     
