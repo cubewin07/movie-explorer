4b8307867831da549c05d646f94378299511db32
cubewin <tanthang071208@gmail.com>
2025-09-29 15:03:15 +0700

    Attach username to attribute

--- Backend/src/main/java/com/Backend/websocket/interceptor/WebsocketHandshaker.java
+++ Backend/src/main/java/com/Backend/websocket/interceptor/WebsocketHandshaker.java
@@ -4,13 +4,22 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+import com.Backend.exception.UserNotFoundException;
+import com.Backend.services.user_service.model.User;
+import com.Backend.services.user_service.repository.UserRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 import java.util.Map;
 
+@RequiredArgsConstructor
 public class WebsocketHandshaker implements HandshakeInterceptor {
     
+    private final UserRepository userRepository;
+
     @Override
     public boolean beforeHandshake(
         @NonNull ServerHttpRequest request, 
@@ -20,7 +29,9 @@ public class WebsocketHandshaker implements HandshakeInterceptor {
     ) throws Exception {
         String uri = request.getURI().toString();
         String userId = uri.split("=")[1];
-        attributes.put("userId", userId);
+        User user = userRepository.findById(Long.parseLong(userId)).orElseThrow(() -> new UserNotFoundException("User not found"));
+        String username = user.getUsername();
+        attributes.put("username", username);
         return true;
     }
 
