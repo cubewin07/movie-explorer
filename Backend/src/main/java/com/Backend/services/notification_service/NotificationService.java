fc26947a3304ff681de865bba6f8bfe8bb534db8
cubewin <tanthang071208@gmail.com>
2025-09-29 19:32:16 +0700

    Create notification logic

--- Backend/src/main/java/com/Backend/services/notification_service/NotificationService.java
+++ Backend/src/main/java/com/Backend/services/notification_service/NotificationService.java
@@ -1,5 +1,30 @@
 package com.Backend.services.notification_service;
 
+import java.time.LocalDateTime;
+
+import com.Backend.services.user_service.model.User;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+@Service
+@RequiredArgsConstructor
 public class NotificationService {
     
+    private final NotificationRepo notificationRepo;
+    private final SimpMessagingTemplate template;
+    
+    public void createNotification(User user, String type, Long relatedId, String message) {
+        Notification notification = Notification.builder()
+            .user(user)
+            .type(type)
+            .relatedId(relatedId)
+            .message(message)
+            .isRead(false)
+            .createdAt(LocalDateTime.now())
+            .build();
+        notificationRepo.save(notification);
+        template.convertAndSend("/topic/notifications/" + user.getId(), notification);
+    }
 }
