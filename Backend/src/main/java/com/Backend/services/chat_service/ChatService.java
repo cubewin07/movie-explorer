de2ce730689cb7021807861b19d3d2c7d04c7256
cubewin <tanthang071208@gmail.com>
2025-09-30 20:38:19 +0700

    Refine chat service

--- Backend/src/main/java/com/Backend/services/chat_service/ChatService.java
+++ Backend/src/main/java/com/Backend/services/chat_service/ChatService.java
@@ -1,9 +1,15 @@
 package com.Backend.services.chat_service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.Backend.services.chat_service.message.Message;
+import com.Backend.services.chat_service.message.MessageRepository;
 import com.Backend.services.user_service.model.User;
 
 import jakarta.transaction.Transactional;
@@ -14,24 +20,35 @@ import lombok.RequiredArgsConstructor;
 public class ChatService {
 
     private final ChatRepository chatRepository;
+    private final MessageRepository messageRepository;
     
     @Transactional
     public Chat createChat(User user1, User user2) {
         Chat chat = Chat.builder()
-            .participants(Set.of(user1, user2))
             .build();
+        chat.addParticipant(user1);
+        chat.addParticipant(user2);
         return chatRepository.save(chat);
     }
 
     public Set<Chat> getChats(User user) {
-        return user.getChats();
+        return chatRepository.findByParticipantsContaining(user);
+    }
+
+    public Page<Message> getMessages(Long chatId, Pageable pageable) {
+        return messageRepository.findByChatIdOrderByCreatedAtDesc(chatId, pageable);
+    }
+
+    public Page<Message> getMessages(Long chatId) {
+        Pageable pageable = PageRequest.of(0, 20);
+        return messageRepository.findByChatIdOrderByCreatedAtDesc(chatId, pageable);
     }
 
     @Transactional
     public Chat createGroupChat(Set<User> users) {
         Chat chat = Chat.builder()
-            .participants(users)
             .build();
+        users.forEach(user -> chat.addParticipant(user));
         return chatRepository.save(chat);
     }
 
