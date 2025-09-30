20d0244bb51da2c24813f7eaabe39f025e1c6b8b
cubewin <tanthang071208@gmail.com>
2025-09-30 20:09:54 +0700

    Adding methods in chat repository

--- Backend/src/main/java/com/Backend/services/chat_service/ChatRepository.java
+++ Backend/src/main/java/com/Backend/services/chat_service/ChatRepository.java
@@ -2,9 +2,12 @@ package com.Backend.services.chat_service;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 
 import com.Backend.services.user_service.model.User;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 import java.util.Set;
 import java.util.Optional;
@@ -13,6 +16,15 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
     @NonNull
     @EntityGraph(attributePaths = {"participants", "messages"})
     Optional<Chat> findById(@NonNull Long id);
+
+    @Query("""
+    select c from Chat c
+    join c.participants p1
+    join c.participants p2
+    where p1 = :user1 and p2 = :user2
+""")
+
+    Set<Chat> findChatByParticipants(@NonNull @Param("user1") User user1, @NonNull @Param("user2") User user2);
 
 
     Set<Chat> findByParticipantsContaining(User user);
