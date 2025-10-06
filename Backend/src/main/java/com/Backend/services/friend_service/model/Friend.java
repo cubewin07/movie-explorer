4ead232d9c42abd78461c79f51efdeb949ffbf6b
cubewin <tanthang071208@gmail.com>
2025-10-06 11:35:00 +0700

    chnage friend's foreign key to lazy proxy

--- Backend/src/main/java/com/Backend/services/friend_service/model/Friend.java
+++ Backend/src/main/java/com/Backend/services/friend_service/model/Friend.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.EnumType;
@@ -38,12 +39,12 @@ import lombok.NoArgsConstructor;
 public class Friend {
 
     @Id
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user1_id")
     private User user1;
 
     @Id
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user2_id")
     private User user2;
     
