e43d65c04bbed17bbbe292ab24280d73cab63312
cubewin <tanthang071208@gmail.com>
2025-10-07 10:07:04 +0700

    Adding validation for register dto

--- Backend/src/main/java/com/Backend/services/user_service/model/DTO/RegisterDTO.java
+++ Backend/src/main/java/com/Backend/services/user_service/model/DTO/RegisterDTO.java
@@ -1,8 +1,20 @@
 package com.Backend.services.user_service.model.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record RegisterDTO(
+    @NotNull(message = "Username cannot be null")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     String username,
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
     String email,
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
     String password
 )
 {}
