6aa16859da48e26b47c55eb58ca4e67e470691af
cubewin <tanthang071208@gmail.com>
2025-10-07 20:29:15 +0700

    Enable caching

--- Backend/src/main/java/com/Backend/BackendsApplication.java
+++ Backend/src/main/java/com/Backend/BackendsApplication.java
@@ -1,9 +1,11 @@
 package com.Backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCaching	
 public class BackendsApplication {
 
 	public static void main(String[] args) {
