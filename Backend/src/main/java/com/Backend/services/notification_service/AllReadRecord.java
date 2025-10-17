ae09e977d7b566067f2a7f90de8fb947cddc748f
cubewin <tanthang071208@gmail.com>
2025-10-17 09:34:23 +0700

    Created endpoint for mark all as read

---
+++ Backend/src/main/java/com/Backend/services/notification_service/AllReadRecord.java
@@ -0,0 +1,8 @@
+package com.Backend.services.notification_service;
+
+import java.util.List;
+
+public record AllReadRecord(
+    List<Long> ids
+)
+{}
---
+++ Backend/src/main/java/com/Backend/services/notification_service/NotificationController.java
@@ -0,0 +1,22 @@
+package com.Backend.services.notification_service;
+
+import com.Backend.services.user_service.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @PostMapping("/allRead")
+    public ResponseEntity<Map<String, String>> markAllAsRead(@AuthenticationPrincipal User user, @RequestBody AllReadRecord listOfIds) {
+        notificationService.markNotificationAsRead(user, listOfIds.ids());
+        return ResponseEntity.ok(Map.of("message", "Marked all notifications as read"));
+    }
+}
