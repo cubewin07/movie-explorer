a9935b3a86771f40be4eb3fe242b0220d9393b39
cubewin <tanthang071208@gmail.com>
2025-10-17 14:23:23 +0700

    Create hooks for notification

---
+++ Frontend/src/Hooks/notification/useNotificationActions.js
@@ -0,0 +1,56 @@
+import instance from "@/lib/instance";
+
+export const useNotificationActions = () => {
+  const queryClient = useQueryClient();
+
+  // Mark notification as read
+  const markAsRead = useMutation({
+    mutationFn: async (notificationId) => {
+      const response = await instance.put(`/notifications/read/${notificationId}`);
+      return response.data;
+    },
+    onSuccess: () => {
+      // Invalidate relevant queries
+      queryClient.invalidateQueries({ queryKey: ['notifications'] });
+    },
+    onError: (error) => {
+      console.error('Error marking notification as read:', error);
+    }
+  });
+
+  // Delete notification
+//   const deleteNotification = useMutation({
+//     mutationFn: async (notificationId) => {
+//       const response = await instance.delete('/notifications/delete', {
+//         params: { id: notificationId }
+//       });
+//       return response.data;
+//     },
+//     onSuccess: () => {
+//       // Invalidate relevant queries
+//       queryClient.invalidateQueries({ queryKey: ['notifications'] });
+//     },
+//     onError: (error) => {
+//       console.error('Error deleting notification:', error);
+//     }
+//   });
+
+    const markAllAsRead = useMutation({
+    mutationFn: async (listOfIds) => {
+      const response = await instance.put('/notifications/allRead', { ids: listOfIds });
+      return response.data;
+    },
+    onSuccess: () => {
+      // Invalidate relevant queries
+      queryClient.invalidateQueries({ queryKey: ['notifications'] });
+    },
+    onError: (error) => {
+      console.error('Error marking all notifications as read:', error);
+    }
+  });
+
+  return {
+    markAsRead,
+    deleteNotification
+  };
+}
