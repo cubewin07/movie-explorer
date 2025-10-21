acd86874d5816f7dce4133714708a0035de039b1
cubewin <tanthang071208@gmail.com>
2025-10-21 10:58:04 +0700

    Manually evict friends cache wiht key = event.email

--- Backend/src/main/java/com/Backend/websocket/eventListener/UserStatusEventListener.java
+++ Backend/src/main/java/com/Backend/websocket/eventListener/UserStatusEventListener.java
@@ -2,6 +2,8 @@ package com.Backend.websocket.eventListener;
 
 import com.Backend.services.friend_service.service.FriendService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,12 @@ import org.springframework.stereotype.Component;
 public class UserStatusEventListener {
     private final FriendService friendService;
     private final SimpMessagingTemplate messagingTemplate;
+    private final CacheManager cacheManager;
 
     @EventListener
     public void onUserStatusChange(UserStatusEvent event) {
+        Cache friends = cacheManager.getCache("friends");
+        if(friends != null) friends.evict(event.email());
         friendService.getAllFriendsReturnASetOfIds(event.email()).forEach(friendId -> {
             messagingTemplate.convertAndSend(
                     "/topic/friends/status/" + friendId,
