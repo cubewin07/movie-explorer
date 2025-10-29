f3792a6ce87c4e099a3f8cc6e66cb308698451b8
cubewin <tanthang071208@gmail.com>
2025-10-29 10:59:22 +0700

    autowire application event publisher instance

--- Backend/src/main/java/com/Backend/services/friend_service/service/FriendService.java
+++ Backend/src/main/java/com/Backend/services/friend_service/service/FriendService.java
@@ -22,6 +22,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import com.Backend.websocket.eventListener.STOMPEventListener;
 
@@ -39,6 +40,7 @@ public class FriendService {
     private final NotificationService notificationService;
     private final CacheManager cacheManager;
     private final STOMPEventListener eventListener;
+    private final ApplicationEventPublisher publisher;
 
     @Cacheable(value = "friendRequests", key = "'from-' + #id")
     public Set<FriendRequestDTO> getRequestsFromThisUser(Long id) {
