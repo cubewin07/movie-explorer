94e2ae27d3db3e7091e035cc432ec0a867d75da5
cubewin <tanthang071208@gmail.com>
2025-11-03 17:16:52 +0700

    Create new method in message repository for finding message with expected chat id, sender id and is read = false

--- Backend/src/main/java/com/Backend/services/chat_service/message/repository/MessageRepository.java
+++ Backend/src/main/java/com/Backend/services/chat_service/message/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.Backend.services.chat_service.message.repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,6 +12,8 @@ import org.springframework.data.domain.Pageable;
 
 public interface MessageRepository extends JpaRepository<Message, Long>{
     Page<Message> findByChatIdOrderByCreatedAtDesc(Long chatId, Pageable pageable);
+
+    Set<Message> findByChatIdAndSenderIdAndReadFalse(Long chatId, Long senderId);
     
     /**
      * Find the most recent message in a chat
