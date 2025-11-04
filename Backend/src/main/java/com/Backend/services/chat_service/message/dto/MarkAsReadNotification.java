d793d3634f77e40a2a04c969478f3c912f786893
cubewin <tanthang071208@gmail.com>
2025-11-04 08:57:55 +0700

    CReate mark as read notifcation

---
+++ Backend/src/main/java/com/Backend/services/chat_service/message/dto/MarkAsReadNotification.java
@@ -0,0 +1,14 @@
+package com.Backend.services.chat_service.message.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MarkAsReadNotification {
+    private Long chatId;
+    private Long userId;
+    private String senderName;
+    private String type;
+    private String message;
+}
