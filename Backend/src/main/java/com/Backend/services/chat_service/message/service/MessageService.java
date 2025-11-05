f5d79c31ddfcb54a65fafbd4b43cde2918a080cb
cubewin <tanthang071208@gmail.com>
2025-11-05 19:12:57 +0700

    Import Chat that I accidentally deleted back then

--- Backend/src/main/java/com/Backend/services/chat_service/message/service/MessageService.java
+++ Backend/src/main/java/com/Backend/services/chat_service/message/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.Backend.services.chat_service.message.service;
 
 import com.Backend.services.chat_service.message.dto.MarkAsReadNotificationDTO;
+import com.Backend.services.chat_service.model.Chat;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
