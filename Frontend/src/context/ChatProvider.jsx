dd41963463897ac679b73e682e1c5a0f4c8abd1d
cubewin <tanthang071208@gmail.com>
2025-11-06 19:32:34 +0700

    Draft create chat (waiting for posting hook

--- Frontend/src/context/ChatProvider.jsx
+++ Frontend/src/context/ChatProvider.jsx
@@ -10,6 +10,19 @@ function ChatProvider({ children }) {
   const [activeChat, setActiveChat] = useState(null);
   const { stompClientRef } = useWebsocket();
 
+
+  const createChat = (participants) => {
+    // Logic to create a new chat via WebSocket or API
+    // For example, send a message to the server to create a new chat
+    const payload = {}
+    if (stompClientRef.current && stompClientRef.current.connected) {
+        for(const i = 0; i < participants.length; i++) {
+            const id = i + 1;
+            payload['user' + id + 'Id'] = participants[i];
+        }
+    }
+  }
+
   return (
     <ChatContext.Provider value={{ activeChat, setActiveChat }}>
       {children}
