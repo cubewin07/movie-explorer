0fef91e1320d07400142166bb0cfda552985d5e0
cubewin <tanthang071208@gmail.com>
2025-11-13 16:04:05 +0700

    changing endpoint in + adding debug console in use mark message as read

--- Frontend/src/Hooks/chat/useMarkMessageAsRead.js
+++ Frontend/src/Hooks/chat/useMarkMessageAsRead.js
@@ -5,7 +5,7 @@ export default function useMarkMessageAsRead(token) {
     const queryClient = useQueryClient();
     return useMutation({
         mutationFn: async (chatId) => {
-            const response = await instance.post(`/chats/${chatId}/mark-read`, null, {
+            const response = await instance.post(`/messages/mark-as-read`, null, {
                 params: {
                     chatId: chatId
                 }
@@ -15,6 +15,10 @@ export default function useMarkMessageAsRead(token) {
         onSuccess: (data, variables) => {
             queryClient.invalidateQueries({ queryKey: ['chat', variables.chatId, 'messages'] });
             queryClient.invalidateQueries({ queryKey: ['userInfo', token] });
+            console.log(data);
+        },
+        onError: (error) => {
+            console.error("Error marking messages as read:", error);
         }
     })
 }
