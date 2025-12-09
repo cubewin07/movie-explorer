21b8656e9914727a61d1cb69715712b8a2ed53cd
cubewin <tanthang071208@gmail.com>
2025-12-09 19:32:23 +0700

    Draft useChatBox hook

---
+++ Frontend/src/Hooks/useChatBox.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const N8N_URL = "http://localhost:5678/webhook-test/affccf51-4ce6-42d0-8f58-dc843ac5e3ff";
+
+function useChatBox(sessionToken) {
+  const send = async () => {
+    try {
+        const response = await axios.post(N8N_URL, {
+          message: "Hi",
+          sessionToken: localStorage.getItem('sessionToken'),
+        });
+        return response.data;
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  }
+  return { send };
+}
+
+export default useChatBox;
