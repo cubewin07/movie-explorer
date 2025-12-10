336163685464803f2c00c2b896f636f97cc4e42d
cubewin <tanthang071208@gmail.com>
2025-12-10 19:20:34 +0700

    Change to production url

--- Frontend/src/Hooks/useChatBox.js
+++ Frontend/src/Hooks/useChatBox.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const N8N_URL = "http://localhost:5678/webhook-test/affccf51-4ce6-42d0-8f58-dc843ac5e3ff";
+const N8N_URL = "http://localhost:5678/webhook/affccf51-4ce6-42d0-8f58-dc843ac5e3ff";
 
 function useChatBox(sessionToken) {
   const send = async (message) => {
