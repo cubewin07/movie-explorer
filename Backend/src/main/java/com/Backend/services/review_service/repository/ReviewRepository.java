1069b71d3256f8ac9d03fe1fb1789a6dae0b5ef5
cubewin <tanthang071208@gmail.com>
2025-12-16 20:23:57 +0700

    Change the return type to page

--- Backend/src/main/java/com/Backend/services/review_service/repository/ReviewRepository.java
+++ Backend/src/main/java/com/Backend/services/review_service/repository/ReviewRepository.java
@@ -2,8 +2,12 @@ package com.Backend.services.review_service.repository;
 
 import com.Backend.services.FilmType;
 import com.Backend.services.review_service.model.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    Review findByFilmIdAndType(Long filmId, FilmType filmType);
+    Page<Review> findByFilmIdAndType(Long filmId, FilmType filmType, Pageable pageable);
 }
