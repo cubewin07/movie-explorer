c93e8301cc888df88fc48226ad065d5d8b098939
cubewin <tanthang071208@gmail.com>
2025-12-16 20:35:33 +0700

    Implement the paginated method to getting reviews

--- Backend/src/main/java/com/Backend/services/review_service/service/ReviewService.java
+++ Backend/src/main/java/com/Backend/services/review_service/service/ReviewService.java
@@ -5,6 +5,10 @@ import com.Backend.services.review_service.model.Review;
 import com.Backend.services.review_service.repository.ReviewRepository;
 import com.Backend.services.user_service.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +16,8 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
     private final ReviewRepository reviewRepository;
 
-    public void getReviewsByFilmId(Long filmId, FilmType filmType, User user) {
+    public void getReviewsByFilmId(Long filmId, FilmType filmType, User user, int page) {
+        Pageable pageable = PageRequest.of(page, 20, Sort.by("createdAt").descending());
+        Page<Review> reviews = reviewRepository.findByFilmIdAndType(filmId, filmType, pageable);
     }
 }
