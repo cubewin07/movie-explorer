ad9ea02380aa18d6dc895b77df0608bd8370123c
cubewin <tanthang071208@gmail.com>
2025-12-16 19:48:57 +0700

    Draft the first endpoint in review controller

---
+++ Backend/src/main/java/com/Backend/services/review_service/controller/ReviewController.java
@@ -0,0 +1,24 @@
+package com.Backend.services.review_service.controller;
+
+import com.Backend.services.review_service.model.Review;
+import com.Backend.services.user_service.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+    @GetMapping()
+    public ResponseEntity<List<Review>> getReviews(
+            @RequestParam("filmId") Long filmId,
+            @RequestParam("type") String type,
+            @AuthenticationPrincipal User user){
+        return ResponseEntity.ok(null);
+    }
+}
