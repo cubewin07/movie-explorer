d9cc9f775f6dbd1fd4acb048d0ecf3dbc816b34b
cubewin <tanthang071208@gmail.com>
2025-12-17 20:49:30 +0700

    Added likeByMe to ReviewsDTO

--- Backend/src/main/java/com/Backend/services/review_service/model/ReviewsDTO.java
+++ Backend/src/main/java/com/Backend/services/review_service/model/ReviewsDTO.java
@@ -13,9 +13,10 @@ public class ReviewsDTO {
     private String content;
     private long replyCount;
     private SimpleUserDTO user;
+    private Boolean likedByMe;
     private LocalDateTime createdAt;
 
-    public static ReviewsDTO fromReview(Review review) {
+    public static ReviewsDTO fromReview(Review review, Boolean likedByMe) {
         Long userId = review.getUser().getId();
         String userEmail = review.getUser().getEmail();
         String userName = review.getUser().getRealUsername();
@@ -25,6 +26,7 @@ public class ReviewsDTO {
                 .content(review.getContent())
                 .replyCount(review.getReplyCount())
                 .user(simpleUserDTO)
+                .likedByMe(likedByMe)
                 .createdAt(review.getCreatedAt())
                 .build();
     }
