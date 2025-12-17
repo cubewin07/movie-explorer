cdea4ebf8c9bbe33d5b0f404a5d6aae0733d9d01
cubewin <tanthang071208@gmail.com>
2025-12-17 09:34:05 +0700

    Create missing findByUser in review repository

--- Backend/src/main/java/com/Backend/services/review_service/repository/ReviewRepository.java
+++ Backend/src/main/java/com/Backend/services/review_service/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.Backend.services.review_service.repository;
 
 import com.Backend.services.FilmType;
 import com.Backend.services.review_service.model.Review;
+import com.Backend.services.user_service.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,5 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByFilmIdAndType(Long filmId, FilmType filmType, Pageable pageable);
     Page<Review> findByAnswerTo_Id(Long reviewId, Pageable pageable);
+    List<Review> findByUserOrderByCreatedAtDesc(User user);
 }
