0d47015871051de3641ffbc6ebc644bd4b614adc
cubewin <tanthang071208@gmail.com>
2025-12-17 09:48:07 +0700

    Create createReviewRequest

---
+++ Backend/src/main/java/com/Backend/services/review_service/model/CreateReviewRequest.java
@@ -0,0 +1,13 @@
+package com.Backend.services.review_service.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CreateReviewRequest {
+    private String content;
+    private long filmId;
+    private String type;
+    private long replyToId;
+}
