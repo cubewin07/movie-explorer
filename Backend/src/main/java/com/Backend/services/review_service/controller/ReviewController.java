d26415abd19aaf50d2b499eb7ab7b66082442108
cubewin <tanthang071208@gmail.com>
2025-12-17 10:03:28 +0700

    Create posting reply endpoint

--- Backend/src/main/java/com/Backend/services/review_service/controller/ReviewController.java
+++ Backend/src/main/java/com/Backend/services/review_service/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.Backend.services.review_service.controller;
 
 import com.Backend.services.FilmType;
+import com.Backend.services.review_service.model.CreateReplyRequest;
 import com.Backend.services.review_service.model.Review;
 import com.Backend.services.review_service.model.ReviewsDTO;
 import com.Backend.services.review_service.service.ReviewService;
@@ -47,6 +48,13 @@ public class ReviewController {
             @RequestBody Review review,
             @AuthenticationPrincipal User user){
         return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/reply")
+    public ResponseEntity<ReviewsDTO> createReply(
+            @RequestBody CreateReplyRequest request,
+            @AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(reviewService.createReply(request, user));
     }
 
     @DeleteMapping("/delete/{reviewId}")
