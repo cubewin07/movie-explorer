e8552c0a3a3fafc6e2e9e4445d7bd81e0b51fcd0
cubewin <tanthang071208@gmail.com>
2025-12-17 20:46:59 +0700

    Create voteRepository

---
+++ Backend/src/main/java/com/Backend/services/review_service/model/vote/VoteRepository.java
@@ -0,0 +1,13 @@
+package com.Backend.services.review_service.model.vote;
+
+
+import com.Backend.services.user_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    List<Vote> findByUserAndReview_IdIn(User user, List<Long> reviewIds );
+}
