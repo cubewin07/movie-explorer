8aa8bcb5125c3ffd22e2362eef4f06a2920f314c
cubewin <tanthang071208@gmail.com>
2025-12-18 10:29:16 +0700

    Create updateVote service

--- Backend/src/main/java/com/Backend/services/review_service/model/vote/VoteService.java
+++ Backend/src/main/java/com/Backend/services/review_service/model/vote/VoteService.java
@@ -1,5 +1,7 @@
 package com.Backend.services.review_service.model.vote;
 
+import com.Backend.services.review_service.model.Review;
+import com.Backend.services.review_service.repository.ReviewRepository;
 import com.Backend.services.user_service.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,35 @@ import java.util.List;
 @RequiredArgsConstructor
 public class VoteService {
     private final VoteRepository voteRepository;
+    private final ReviewRepository reviewRepository;
 
     public List<Vote> voteByUserAndReviewIds(User user, List<Long> reviewIds) {
         return voteRepository.findByUserAndReview_IdIn(user, reviewIds);
     }
 
     public void updateVote(VoteStateRequest request, User user) {
+        Vote existingVote = voteRepository.findByUserAndReview_Id(user, request.getReviewId());
+        Review review = reviewRepository.findById(request.getReviewId()).orElseThrow();
 
+        //   vote exists
+        if(existingVote != null) {
+            if(existingVote.getValue() == request.getValue()) return;
+            if(existingVote.getValue() == 0) {
+                voteRepository.delete(existingVote);
+            }
+            existingVote.setValue(request.getValue());
+            review.setScore(review.getScore() + (request.getValue() - existingVote.getValue()));
+            voteRepository.save(existingVote);
+            reviewRepository.save(review);
+        }
+
+        // create a vote
+        Vote vote = Vote.builder()
+                .user(user)
+                .value(request.getValue())
+                .review(review)
+                .build();
+        voteRepository.save(vote);
     }
 
 }
