12750acf0e1f13969f35dc0eebdb0ab1c2acc9dd
cubewin <tanthang071208@gmail.com>
2025-12-24 10:44:32 +0700

    Create episode metadata class

---
+++ Backend/src/main/java/com/Backend/services/review_service/model/EpisodeMetadata.java
@@ -0,0 +1,12 @@
+package com.Backend.services.review_service.model;
+
+import lombok.Data;
+import lombok.Builder;
+
+@Data
+@Builder
+public class EpisodeMetadata {
+    private String episodeId;
+    private int seasonNumber;
+    private int episodeNumber;
+}
