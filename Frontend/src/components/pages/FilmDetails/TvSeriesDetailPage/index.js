ac465b64ad832595ca1929aca4ffb40604f7ccb9
cubewin <tanthang071208@gmail.com>
2025-12-24 09:49:05 +0700

    Delete the old series detail page + add index and modify routert to adapt to the change

---
+++ Frontend/src/components/pages/FilmDetails/TvSeriesDetailPage/index.js
@@ -0,0 +1,5 @@
+export {default as TvSeriesDetailPage} from './TvSeriesDetailPage';
+export {default} from './TvSeriesDetailPage';
+export {default as SeasonsSection} from './SeasonsSection';
+export {default as SeriesReviewsSection} from './SeriesReviewsSection';
+export {useSeriesData} from './useSeriesData';
