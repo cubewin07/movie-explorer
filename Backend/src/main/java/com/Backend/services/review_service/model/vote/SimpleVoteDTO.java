a714f6dc7962e53c13db1946b65deba2107769a6
cubewin <tanthang071208@gmail.com>
2025-12-26 16:31:01 +0700

    Adding no-args contructor for simpleVoteDTO so that kryo can deserialize

--- Backend/src/main/java/com/Backend/services/review_service/model/vote/SimpleVoteDTO.java
+++ Backend/src/main/java/com/Backend/services/review_service/model/vote/SimpleVoteDTO.java
@@ -1,10 +1,14 @@
 package com.Backend.services.review_service.model.vote;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimpleVoteDTO {
     private Long id;
     private int value;
