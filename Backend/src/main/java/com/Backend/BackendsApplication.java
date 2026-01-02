572dd83926d5fb1fb686bd1ab8b65cb0747c17fa
cubewin <tanthang071208@gmail.com>
2026-01-02 20:58:08 +0700

    Disable database

--- Backend/src/main/java/com/Backend/BackendsApplication.java
+++ Backend/src/main/java/com/Backend/BackendsApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {
     org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class
+    org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
 })
 // @EnableCaching
 public class BackendsApplication {
