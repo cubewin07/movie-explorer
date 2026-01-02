85c55657dd7552259ca46b5aaaaa2538de05738b
cubewin <tanthang071208@gmail.com>
2026-01-02 21:09:55 +0700

    Create simple cache manager

--- Backend/src/main/java/com/Backend/BackendsApplication.java
+++ Backend/src/main/java/com/Backend/BackendsApplication.java
@@ -2,6 +2,9 @@ package com.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {
     org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
@@ -9,11 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
     org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
     org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
 })
-// @EnableCaching
+@EnableCaching
 public class BackendsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendsApplication.class, args);
+	}
+
+	@Bean
+	public ConcurrentMapCacheManager cacheManager() {
+		return new ConcurrentMapCacheManager("users", "movies", "reviews", "notifications");
 	}
 
 }
