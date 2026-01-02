e696f5a381ee15c7cb38db5b520534fdf80fc709
cubewin <tanthang071208@gmail.com>
2026-01-02 20:33:32 +0700

    Disable redis

--- Backend/src/main/java/com/Backend/BackendsApplication.java
+++ Backend/src/main/java/com/Backend/BackendsApplication.java
@@ -1,11 +1,10 @@
 package com.Backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableCaching
+// @EnableCaching
 public class BackendsApplication {
 
 	public static void main(String[] args) {
